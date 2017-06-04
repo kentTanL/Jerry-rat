14b14fa54d0f27a26ede5c8a1b191fc49865f5d4
kentTanL <894690230@qq.com>
2017-06-04 14:06:48 +0800

    ex01

--- src/org/jettyrat/FirstClass.java
+++ src/org/jettyrat/FirstClass.java
@@ -1,9 +1,9 @@
 package org.jettyrat;
 
 public class FirstClass {
-	
-	public static void main(String[] args) {
-		System.out.println("Hello Jeety");
-	}
-	
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jeety");
+    }
+
 }
